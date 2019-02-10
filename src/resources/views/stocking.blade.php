0dbfa52d702a6b2354d80aa11da84f00fba798b1
Craig <craig.t.edwards@googlemail.com>
2019-02-10 19:22:28 +0000

    Added filter to stock query to only count outstanding contracts...whoops

--- src/resources/views/stocking.blade.php
+++ src/resources/views/stocking.blade.php
@@ -27,6 +27,7 @@ data-id="{{auth()->user()->character->corporation_id}}">{{ trans('web::seat.unkn
                 <th>Fit Name</th>
                 <th>Min</th>
                 <th>Stock</th>
+                <th>Contract Title</th>
                 <th class="pull-right">Option</th>
              </tr>
              </thead>
@@ -43,6 +44,7 @@ data-id="{{auth()->user()->character->corporation_id}}">{{ trans('web::seat.unkn
                  </td>
                  <td>{{ $item['minlvl'] }}</td>
                  <td>{{ $item['stock'] }}</td>
+                 <th>{{ $item['shiptype'] }} {{ $item['fitname'] }}</th>
                  <td class="no-hover pull-right">
                      
                      @if (auth()->user()->has('whtools.stockedit', false)) 
