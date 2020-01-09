69a23e1fdde1f543f526b0e0379672d0c61921cb
C Edwards <craig.t.edwards@googlemail.com>
2020-01-09 01:18:43 +0000

    Update WHToolsController.php
    Add a count of stocked contracts made my corp members not on behalf of the corp

--- src/Http/Controllers/WHToolsController.php
+++ src/Http/Controllers/WHToolsController.php
@@ -82,7 +82,14 @@ class WHtoolsController extends FittingController
         
         foreach($stocklvllist as $stocklvl){
             $ship = InvType::where('typeName', $stocklvl->fitting->shiptype)->first();
-           
+            
+            //Contracts made to the corp but by corp members not on behalf of the corp
+            $member_stock_contracts = ContractDetail::where('issuer_corporation_id','=',$corporation_id)
+                ->where('title', 'LIKE', '%'.($stocklvl->fitting->shiptype).' '.trim($stocklvl->fitting->fitname).'%')
+                ->where('for_corporation', '=', '0')
+                ->where('status','LIKE','outstanding')
+                ->get();
+            //Contracts made to the corp by corp members on behalf of the corp
             $stock_contracts = ContractDetail::where('issuer_corporation_id','=',$corporation_id)
                 ->where('title', 'LIKE', '%'.($stocklvl->fitting->shiptype).' '.trim($stocklvl->fitting->fitname).'%')
                 ->where('for_corporation', '=', '1')
@@ -99,6 +106,7 @@ class WHtoolsController extends FittingController
                 'id' =>  $stocklvl->id,
                 'minlvl' =>  $stocklvl->minLvl,
                 'stock' =>  $stock_contracts->count(),
+                'member_stock' =>  $member_stock_contracts->count(),
                 'fitting_id' =>  $stocklvl ->fitting_id,
                 'fitname' => $stocklvl->fitting->fitname,
                 'shiptype' =>$stocklvl->fitting->shiptype,
