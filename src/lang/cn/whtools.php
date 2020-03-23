af9e7bfa0452eba270df9f3c4ea3b86c8cb855cd
神楽坂·喵 <yuronglong1996@gmail.com>
2020-03-23 18:01:33 +0800

    add Chinese Translation

--- src/lang/cn/whtools.php
+++ src/lang/cn/whtools.php
@@ -21,12 +21,65 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 return [
 
-    // Language strings for your app go here
+    // 菜单
     'name' => '虫洞工具集',
     'stocking' => '战备',
     'bluetax' => '蓝牌子税',
-    'skillcheck' => '技能检查',
-    'certificates' => '证书'
+    'certificates' => '证书',
 
+    // Blue Tex
+    'bluesalesbydate' => '蓝牌子销售',
+    'bluetotals' => '蓝牌子销售总额',
+    'bluetaxpayments' => '蓝牌子税额',
+    'bluetaxpaymentstotals' => '蓝牌子税总额',
+    'seller' =>'售卖者',
+
+    // Certificates
+    'skillcheck' => '技能检查',
+    'certificateskills' => '证书技能',
+    'createanewcertificate' => '创建一个新的证书',
+    'choosecertificate' => '选择证书....',
+    'requiredlevel' => '需要等级',
+    'characterlevel' => '角色等级',
+    'certificaterank' => '证书等级',
+    'certificatename' => '证书名称',
+    'rank' => '等级',
+    'charactercertificates' => '角色证书',
+    'corporationcertificates' => '公司证书',
+    'charactercertificatesdescription' => '角色证书描述',
+    'corporationcertificatesdescription' => '公司证书描述',
+    'certificatescoverage' => '证书覆盖率 %',
+    'createcertificate' => '创建一个新的证书',
+    'editcert' => '编辑证书',
+    'deletecert' => '删除证书',
+    'showrank' => '显示等级',
+
+    // Stocking
+    'stocklevelsfor' => '战备等级关于',
+    'addstocking' => '添加一个新的战备',
+    'ship' => '舰船',
+    'fitname' => '装配名称',
+    'min' => '最少',
+    'corpstock' => '公司战备',
+    'membersstock' => '成员战备',
+    'contracttitle' => '合同标题',
+    'option' => '操作',
+    'viewfitting' => '查看装配',
+    'editstocking' => '编辑战备',
+    'deletestocking' => '删除战备',
+    'total' => '总计',
+    'selectfit' => 'Which Fit to add to Stock List',
+    'pedittext' => 'Select the fitting to add a stocking level for and enter the minimum stock level.',
+    'submitstock' => 'Submit Stock',
+    'confirm' => 'Are you sure?',
+    'confirmdeletestocking' => 'Are you sure you want to delete this stocking?',
+    'eftfitting' => 'EFT Fitting',
+    'lowslotmodule' => 'Low Slot Module',
+    'midslotmodule' => 'Mid Slot Module',
+    'highslotmodule' => 'High Slot Module',
+    'rigs' => 'Rigs',
+    'subsystems' => 'Subsystems',
+    'dronebay' => 'Drone Bay',
+    'number' => 'Number',
 ];
 
